3a651207b7cd9c2df333edebb2f3956d59cd9e79
Phuc Le <phuclh93@gmail.com>
2021-02-06 11:28:09 -0800

    Moving Forms model, factory, migration to package.

---
+++ src/Models/Contact.php
@@ -0,0 +1,64 @@
+<?php namespace Tipoff\Forms\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Tipoff\Support\Models\BaseModel;
+
+class Contact extends BaseModel
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'emailed_at' => 'datetime',
+        'requested_date' => 'date',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($contact) {
+            if (empty($contact->user_id)) {
+                throw new \Exception('A contact must be made by a user.');
+            }
+            if (empty($contact->location_id)) {
+                throw new \Exception('A contact must be made to a location.');
+            }
+            $contact->generateReferenceNumber();
+        });
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'reference_number';
+    }
+
+    public function generateReferenceNumber()
+    {
+        do {
+            $token = Str::of(Carbon::now('America/New_York')->format('ymdB'))->substr(1, 7) . Str::upper(Str::random(3));
+        } while (self::where('reference_number', $token)->first()); //check if the token already exists and if it does, try again
+
+        $this->reference_number = $token;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function notes()
+    {
+        return $this->morphMany(Note::class, 'noteable');
+    }
+}

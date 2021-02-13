3dbb7ae4081b6097f194694813e00b8935b76441
Drew Roberts <github@drewroberts.com>
2021-02-13 10:17:47 -0500

    Remove policies since not created yet

--- src/FormsServiceProvider.php
+++ src/FormsServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tipoff\Forms;
 
-use Tipoff\Forms\Policies\FormPolicy;
 use Tipoff\Support\TipoffPackage;
 use Tipoff\Support\TipoffServiceProvider;
 
@@ -13,9 +12,6 @@ class FormsServiceProvider extends TipoffServiceProvider
     public function configureTipoffPackage(TipoffPackage $package): void
     {
         $package
-            ->hasPolicies([
-                Form::class => FormPolicy::class,
-            ])
             ->name('forms')
             ->hasConfigFile();
     }

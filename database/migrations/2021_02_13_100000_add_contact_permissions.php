6a3f8ca28ddaa229c36532d696c4bf3828aea3c0
Drew Roberts <github@drewroberts.com>
2021-03-10 12:46:09 -0500

    Add new permissions to migration #23

--- database/migrations/2021_02_13_100000_add_contact_permissions.php
+++ database/migrations/2021_02_13_100000_add_contact_permissions.php
@@ -9,7 +9,10 @@ class AddContactPermissions extends BasePermissionsMigration
     public function up()
     {
         $permissions = [
-            'view contacts' => ['Owner', 'Staff']
+            'view contacts' => ['Owner', 'Staff'],
+            'create contacts' => ['Owner', 'Staff'],
+            'update contacts' => ['Owner', 'Staff'],
+            'delete contacts' => ['Owner', 'Staff'],
         ];
 
         $this->createPermissions($permissions);

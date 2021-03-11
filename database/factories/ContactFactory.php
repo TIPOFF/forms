9adb7bc2436e86889980504bc4494b0e648877e7
Drew Roberts <github@drewroberts.com>
2021-03-10 23:41:33 -0500

    Update contact factory #30

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -9,27 +9,17 @@ use Tipoff\Forms\Models\Contact;
 
 class ContactFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = Contact::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
             'form_type'      => $this->faker->randomElement(['contact', 'reservation', 'parties', 'groups', 'employment']),
-            'user_id'        => randomOrCreate(app('user')),
             'location_id'    => randomOrCreate(app('location')),
+            'email'          => $this->faker->email,
+            'first_name'     => $this->faker->firstName,
+            'last_name'      => $this->faker->lastName,
             'phone'          => '555-555-5555',
-            'participants'   => $this->faker->numberBetween(10, 200),
-            'requested_date' => $this->faker->dateTimeBetween($startDate = 'now', $endDate = '+6 months')->format('Y-m-d'),
             'company_name'   => $this->faker->company,
             'message'        => $this->faker->paragraph,
         ];

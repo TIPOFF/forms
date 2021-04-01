7a4dbe0873b3f5b9a8f80ab536a9946a11aee9ac
Chris H <crishack8@gmail.com>
2021-04-01 17:41:59 -0500

    Add basic controller for review reference

---
+++ resources/views/website/pages/contact.blade.php
@@ -0,0 +1,115 @@
+@extends('website.layout')
+
+@section('amp-scripts')
+    @parent
+    <script async custom-element="amp-recaptcha-input" src="https://cdn.ampproject.org/v0/amp-recaptcha-input-0.1.js"></script>
+@endsection
+
+@section('content')
+    @include('website.subviews.covid-notice')
+
+    <section class="tger-contact">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="tger-contact__inner">
+                        <form
+                            method="post"
+                            action-xhr="{{ $location->submit_form_url }}"
+                            target="_top">
+                            <fieldset>
+                                @csrf
+                                <input
+                                    name="form_type"
+                                    value="contact"
+                                    type="hidden">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">First Name<span class="required">*</span></label>
+                                            <input
+                                                type="text"
+                                                name="name"
+                                                class="form-control"
+                                                placeholder="Enter first name"
+                                                required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">Last Name<span class="required">*</span></label>
+                                            <input
+                                                type="text"
+                                                name="name_last"
+                                                class="form-control"
+                                                placeholder="Enter last name"
+                                                required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">Email<span class="required">*</span></label>
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                class="form-control"
+                                                placeholder="Enter your email"
+                                                required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="">Phone<span class="required">*</span></label>
+                                            <input
+                                                type="tel"
+                                                name="phone"
+                                                class="form-control"
+                                                placeholder="Enter your phone"
+                                                required>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-group">
+                                            <label for="">Message</label>
+                                            <textarea
+                                                name="message"
+                                                class="form-control"
+                                                rows="4"
+                                                placeholder="Enter your message">
+                                                </textarea>
+                                        </div>
+                                        <div class="tger-button-container">
+                                            <input
+                                                value="Submit"
+                                                class="button6"
+                                                type="submit">
+                                        </div>
+                                    </div>
+                                </div>
+                                <amp-recaptcha-input layout="nodisplay"
+                                                     name="recaptcha_token"
+                                                     data-sitekey="{{ config('app.recaptcha_site') }}"
+                                                     data-action="recaptcha_example">
+                                </amp-recaptcha-input>
+                            </fieldset>
+                            <div submit-success>
+                                <template type="amp-mustache">
+                                    Message sent successfully!
+                                </template>
+                            </div>
+                            <div submit-error>
+                                <template type="amp-mustache">
+                                    Submission failed!<br><br>
+                                    @{{ #verifyErrors }}
+                                    <p>@{{ message }}</p>
+                                    @{{ /verifyErrors }}
+                                </template>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ src/Http/Controllers/Web/ContactController.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tipoff\Forms\Http\Controllers\Web;
+
+use Illuminate\Routing\Controller;
+use App\Models\Location;
+use App\Models\Market;
+use DrewRoberts\Media\Models\Image;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function __invoke(Request $request, Market $market = null, Location $location = null)
+    {
+        $current = getCurrentMarket($request);
+
+        // Redirect to the user's current market if they have one set and end up on page in company section
+        if ($current && $request->segment(1) === 'company') {
+            return redirect()->to($current->contact_path);
+        }
+
+        $image = Image::find(40)->url;
+
+        // Show the page in the company section of the site
+        if ($request->segment(1) === 'company') {
+            return view('website.pages.company.choose', [
+                'market' => $current,
+                'html' => null, // set to true if need to use html page instead of AMP
+                'title' => 'Contact Us',
+                'subtitle' => 'Please select your location from the dropdown below:',
+                'cta' => null,
+                'link' => 'contact',
+                'seotitle' => 'Contact Us at The Great Escape Room',
+                'seodescription' => 'Contact Us at The Great Escape Room, a leader in the escape room industry with 12 locations. Visit our website to learn more.',
+                'ogtitle' => 'Contact Us at The Great Escape Room',
+                'ogdescription' => 'Contact Us at The Great Escape Room, a leader in the escape room industry with 12 locations. Visit our website to learn more.',
+                'canonical' => route('contact'),
+                'image' => $image,
+                'ogimage' => $image === null ? url('img/ogimage.jpg') : $image,
+            ]);
+        }
+
+        // Send to correct market if somehow they get to a URL with a different market than where the location belongs
+        if ($location && $market->id !== $location->market_id) {
+            return redirect()->to($location->contact_path);
+        }
+
+        // If there is only one location in the market, remove the trailing location name in the route/URL
+        if ($market->locations_count === 1 && $location) {
+            return redirect()->to($market->contact_path);
+        }
+
+        setCurrentMarket($request, $market);
+
+        // In a multi-location market, ask the user to choose a location
+        if ($market->locations_count !== 1 && ! $location) {
+            return view('website.markets.select', [
+                'market' => $market,
+                'html' => null, // set to true if need to use html page instead of AMP
+                'title' => 'Contact Us',
+                'subtitle' => 'Please select your location from the dropdown below:',
+                'cta' => null,
+                'link' => 'contact',
+                'seotitle' => 'Contact Us at The Great Escape Room',
+                'seodescription' => 'Contact Us at The Great Escape Room, a leader in the escape room industry with 12 locations. Visit our website to learn more.',
+                'ogtitle' => 'Contact Us at The Great Escape Room',
+                'ogdescription' => 'Contact Us at The Great Escape Room, a leader in the escape room industry with 12 locations. Visit our website to learn more.',
+                'canonical' => 'https://thegreatescaperoom.com'.$market->contact_path,
+                'image' => $image,
+                'ogimage' => $image === null ? url('img/ogimage.jpg') : $image,
+            ]);
+        }
+
+        // If end up here, then display the bookings page
+        if (! $location) {
+            $location = $market->locations->first();
+        }
+
+        return view('website.pages.contact', [
+            'market' => $market,
+            'location' => $location,
+            'html' => null,
+            'title' => 'Contact Us at '.$market->title,
+            'subtitle' => 'Please fill in your info below and we\'ll be in touch',
+            'cta' => null,
+            'seotitle' => 'Contact Us at '.$market->title,
+            'seodescription' => $market->title.' has '.$market->rooms->count().' different escape rooms and offers private escape games for groups & parties. Contact us today for more information!',
+            'ogtitle' => 'Contact Us at '.$market->title,
+            'ogdescription' => $market->title.' has '.$market->rooms->count().' different escape rooms and offers private escape games for groups & parties. Contact us today for more information!',
+            'canonical' => 'https://thegreatescaperoom.com'.$location->contact_path,
+            'image' => $image,
+            'ogimage' => $image === null ? url('img/ogimage.jpg') : $image,
+        ]);
+    }
+
+    public function confirmation(Request $request, Market $market, Location $location = null)
+    {
+        $image = Image::find(40)->url;
+        $location = $location ? $location : $market->locations()->first();
+
+        return view('website.pages.confirmation.contact', [
+            'market' => $market,
+            'location' => $location,
+            'noindex' => true, // Prevent Google and other search engines from indexing this page.
+            'title' => 'Thank you for contacting us',
+            'subtitle' => 'We will be in touch soon!',
+            'cta' => null,
+            'canonical' => 'https://thegreatescaperoom.com'.$location->contact_path,
+            'image' => $image,
+            'ogimage' => $image === null ? url('img/ogimage.jpg') : $image,
+        ]);
+    }
+}
